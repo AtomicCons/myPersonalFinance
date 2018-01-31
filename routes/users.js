c64a6a3f96c5071f7d964812ed9fb0f7e16a39ec
Tim <AtomicCons@users.noreply.github.com>
2018-01-30 22:49:27 -0700

    update registration

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user.js');
 /* GET users listing. */
-router.get('/login', function(req, res, next){
+router.get('/login', function(req, res, next) {
   res.render('login');
 });
 router.get('/register', function(req, res){
@@ -12,14 +12,15 @@ router.get('/register', function(req, res){
                           console.log(errors);
 })
 router.post('/register', function(req, res, next){
-  req.assert('username', 'Please choose a longer username').len(4,25);
-  req.assert('password', 'This should be longer. 12 to 100 characters').len(12,100);
+  req.assert('username', 'Username: Please choose a longer username').len(4,25);
+  req.assert('password', 'Password: This should be a bit longer. 12 to 100 characters').len(12,100);
   req.getValidationResult().then(function(result){
     if(!result.isEmpty()){
       var errors = result.array();
       req.session.errors = errors;
       req.session.success = false;
-      console.log(errors)
+      User.findOne({username:username});
+      if(User){req.session.exists = true}
       res.render('register', { errors: req.session.errors,
                                 success: req.session.success,
                                 exists: req.session.exists});
@@ -27,28 +28,48 @@ router.post('/register', function(req, res, next){
       req.session.success = true;
 
       var username = req.body.username;
-      User.findOne({username:username}, function(err, user, next){
-        console.log(user);
-        if(user){
+      User.findOne({username:username}, function(err, User, next){
+        if (User) {
           req.session.exists = true;
           res.render('register', { errors: req.session.errors,
-                                   success: req.session.success,
+                                  success: req.session.success,
                                    exists: req.session.exists})
-        } else if(!user){
-          res.render('setup',{username:req.body.username, password:req.body.password})
+        } else if (!User) {
+          var username = req.body.username
+
+          req.session.setupU = username;
+            console.log(req.session.setupU);
+          res.render('setup',{
+                    username:username, password:req.body.password
+                  })
         }
       })
-
-
     }
   })
+})
+router.post('/setup', function(req, res, next){
+  console.log(req.body.username + ":" + req.session.setupU)
+  if(req.body.username !== req.session.setupU){
+    res.send('You have tried to make innapropirate changes')
+  } else{
+  req.assert('username', 'Username: Please choose a longer username').len(4,25);
+  req.assert('password', 'Password: This should be a bit longer. 12 to 100 characters').len(12,100);
+  req.getValidationResult().then(function(result){
+    if(!result.isEmpty()){
+      req.session.errors = errors;
+      req.session.success = false;
+      User.findOne({username:username});
+      if(User){req.session.exists = true}
+      res.render('setup',{})
+    }
+})
+}
 })
 
 
 
 
-
-router.get('/profile',function(req,res){
+router.get('/profile', function(req, res) {
   res.render('profile');
 })
 
